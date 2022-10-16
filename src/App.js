3dd6ce4813b667fcbe1d823de3cb4c1e264c866b
Rowee Apor <75659500+Rowee13@users.noreply.github.com>
2022-10-16 21:51:31 +0800

    Practice Project 1 - Monster Rolodex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			name: "Rowee",
+		};
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>Hi {this.state.name}</p>
+					<button
+						onClick={() => {
+							this.setState({ name: "Rezyl" });
+						}}
+					>
+						Change Name
+					</button>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
